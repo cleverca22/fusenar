100a039ce056f7bdd383f8165396ea7dcd0600e5
michael bishop <cleverca22@gmail.com>
2016-04-12 17:27:09 -0300

    make the sample data fixed-output

--- inputs.nix
+++ inputs.nix
@@ -2,17 +2,18 @@
 
 stdenv.mkDerivation {
   name = "inputs";
-  outputs = [ "out" "file" ];
   unpackPhase = "true";
   installPhase = ''
-    mkdir -pv $out/dir
+    mkdir -pv $out/dir/dir2/dir3/
     echo file > $out/file
     echo file2 > $out/dir/file2
     ln -sv file $out/link
-    echo file3 > $file
     #ln -sv /dev/null $link
     echo #!/bin/sh > $out/script
     echo 'echo hello world' >> $out/script
     chmod +x $out/script
   '';
+  outputHash = "08kh7flwc9r3ihgz288mhyvgl2nlnqjrb3dflx27mm8ffs1bkz06";
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
 }
